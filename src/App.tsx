b5e78e87e8941e31d2a4eb2ec246fa721c7c8fb6
Juan Maurente <juanmaurente@gmail.com>
2023-04-01 11:31:10 +1100

    HIGHLIGHT THE SELECTED GENRE

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
 			<Show above='lg'>
 				<GridItem area='aside' paddingX={5}>
 					<GenreList
+						selectedGenre={selectedGenre}
 						onSelectGenre={(genre) => setSelectedGenre(genre)}
 					/>
 				</GridItem>
